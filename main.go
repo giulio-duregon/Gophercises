e5a240f05875332c6087237c5d369d46da5ebf43
Gduregon <gjd9961@nyu.edu>
2023-06-10 13:23:38 -0400

    Added second exercise, URL parsing for HTTP handler routing

--- main.go
+++ main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"Gophercises/src/quizGame"
+	"Gophercises/src/urlShortener"
+	"flag"
 )
 
+const (
+	QuizGame int = iota + 1
+	URLShortener
+)
+
+func exerciseSelector() {
+
+}
+
 func main() {
-	quizGame.QuizProgram()
+	program := flag.Int("p", 1, "Selects the exercise that you want to run, 1-based indexing")
+	flag.Parse()
+	switch *program {
+	case QuizGame:
+		quizGame.QuizProgram()
+	case URLShortener:
+		urlShortener.UrlShortProgram()
+	}
+
 }
---
+++ src/urlShortener/urlShortener.go
@@ -0,0 +1,105 @@
+package urlShortener
+
+import (
+	"fmt"
+	yaml "gopkg.in/yaml.v3"
+	"net/http"
+)
+
+type PathUrl struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
+
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if dest, ok := pathsToUrls[path]; ok {
+			http.Redirect(w, r, dest, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(w, r)
+	}
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//   - path: /some-path
+//     url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+
+func parseYAML(yml []byte) (paths []PathUrl, err error) {
+	err = yaml.Unmarshal(yml, &paths)
+	return
+}
+
+func buildMapFromYAML(paths []PathUrl) (mappedPaths map[string]string) {
+	mappedPaths = make(map[string]string)
+	for _, ymlPath := range paths {
+		mappedPaths[ymlPath.Path] = ymlPath.URL
+	}
+	return
+}
+
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	paths, err := parseYAML(yml)
+	if err != nil {
+		return nil, err
+	}
+	mappedPaths := buildMapFromYAML(paths)
+	return MapHandler(mappedPaths, fallback), err
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
+
+func UrlShortProgram() {
+
+	mux := defaultMux()
+
+	// Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := MapHandler(pathsToUrls, mux)
+
+	// Build the YAMLHandler using the mapHandler as the
+	// fallback
+	yml := `
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution
+`
+	yamlHandler, err := YAMLHandler([]byte(yml), mapHandler)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", yamlHandler)
+}
